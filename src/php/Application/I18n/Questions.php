cbe0967e4cadc4d7eb8addb64222bf162424a38c
samizdam <samizdam@inbox.ru>
2022-11-12 19:05:03 +0300

    Clean up code

--- src/php/Application/I18n/Questions.php
+++ src/php/Application/I18n/Questions.php
@@ -8,8 +8,5 @@ class Questions
 {
     public const THROW_DICES_PLAYER = 'Бросай кости, игрок %s!';
     public const WHAT_DO_YOUR_DO    = 'Что будешь делать?';
-    public const CHOICE_CASTLE      = 'Выбери крепость';
-    public const CHOICE_ARMY        = 'Выбери армию';
-    public const CHOICE_TARGET      = 'Выбери куда двигать';
     public const CHOISE_HOW_MATCH   = 'Сколько двигать [1-%d]';
 }
